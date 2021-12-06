2fa119347aff584bae5bb16791ec571f063c1d16
Bernardino Domínguez <bernardino.dominguez@nexthink.com>
2021-12-06 14:26:56 +0100

    cleaning day 6

--- day6/day6.py
+++ day6/day6.py
@@ -6,42 +6,37 @@ def read_file(filename):
 
 
 def iterate_numbers(numbers):
-    zeros = len([x for x in numbers if x == 0])
-    numbers = [x - 1 if x > 0 else 6 for x in numbers]
-    numbers = numbers + [8 for _ in range(zeros)]
+    zeros = sum([y for x, y in numbers if x == 0])
+    numbers = [(x - 1, y) if x > 0 else (6, y) for x, y in numbers]
+    numbers = [(x, y) for (x, y) in
+               [(number, sum([y for x, y in numbers if x == number])) for number in
+                range(9)] if y != 0]
+    if zeros != 0:
+        numbers.append((8, zeros))
     return numbers
 
 
-def iterate_numbers_with_cardinality(numbers_with_cardinality):
-    zeros = sum([y for x, y in numbers_with_cardinality if x == 0])
-    numbers_with_cardinality = [(x - 1, y) if x > 0 else (6, y) for x, y in numbers_with_cardinality]
-    numbers_with_cardinality = [(x, y) for (x, y) in
-                                [(number, sum([y for x, y in numbers_with_cardinality if x == number])) for number in
-                                 range(9)] if y != 0]
-    if zeros != 0:
-        numbers_with_cardinality.append((8, zeros))
-    return numbers_with_cardinality
+def process(numbers, iterations):
+    for i in range(iterations):
+        numbers = iterate_numbers(numbers)
+    return sum([y for (x, y) in numbers])
 
 
-def part_one(numbers_with_cardinality):
-    for i in range(80):
-        numbers_with_cardinality = iterate_numbers_with_cardinality(numbers_with_cardinality)
-    print(sum([y for (x, y) in numbers_with_cardinality]))
+def part_one(numbers):
+    print(process(numbers, 80))
 
 
-def part_two(numbers_with_cardinality):
-    for i in range(256):
-        numbers_with_cardinality = iterate_numbers_with_cardinality(numbers_with_cardinality)
-    print(sum([y for (x, y) in numbers_with_cardinality]))
+def part_two(numbers):
+    print(process(numbers, 256))
 
 
 def main():
     lines = read_file("input.txt")
     numbers = list(map(int, lines[0].split(",")))
-    numbers_with_cardinality = [(x, y) for (x, y) in
-                                [(number, len([x for x in numbers if x == number])) for number in range(6)] if y != 0]
-    part_one(numbers_with_cardinality)
-    part_two(numbers_with_cardinality)
+    numbers = [(x, y) for (x, y) in
+               [(number, len([x for x in numbers if x == number])) for number in range(6)] if y != 0]
+    part_one(numbers)
+    part_two(numbers)
 
 
 main()
