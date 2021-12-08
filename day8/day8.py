ea896302cb0c8cbf0672cdcc229f407bcfa89597
Bernardino Domínguez <bernardino.dominguez@nexthink.com>
2021-12-08 12:59:00 +0100

    day8 refactor

--- day8/day8.py
+++ day8/day8.py
@@ -30,61 +30,82 @@ def sort(letters):
     return ''.join(sorted(letters))
 
 
+def decode_wires_for_unique(left):
+    association = {[sort(left_digit) for left_digit in left if len(left_digit) == 2][0]: 1,
+                   [sort(left_digit) for left_digit in left if len(left_digit) == 4][0]: 4,
+                   [sort(left_digit) for left_digit in left if len(left_digit) == 3][0]: 7,
+                   [sort(left_digit) for left_digit in left if len(left_digit) == 7][0]: 8}
+
+    inverse_association = {1: [sort(left_digit) for left_digit in left if len(left_digit) == 2][0],
+                           4: [sort(left_digit) for left_digit in left if len(left_digit) == 4][0],
+                           7: [sort(left_digit) for left_digit in left if len(left_digit) == 3][0],
+                           8: [sort(left_digit) for left_digit in left if len(left_digit) == 7][0]}
+
+    return association, inverse_association
+
+
+def assign_and_remove(candidates, association, inverse_association, wire, number):
+    association[wire] = number
+    inverse_association[number] = wire
+    candidates.remove(wire)
+
+    return candidates, association, inverse_association
+
+
+def process_with_rule(candidates, rule, inverse_association):
+    return [candidate for candidate in candidates if rule(candidate, inverse_association)][0]
+
+
+def decode_wires_for_length_six(left, association, inverse_association):
+    digits_with_six_wires = [sort(left_digit) for left_digit in left if len(left_digit) == 6]
+
+    six = process_with_rule(digits_with_six_wires,
+                            lambda candidate, inverse_map: True if inverse_map[1][0] not in candidate or inverse_map[1][
+                                1] not in candidate else False, inverse_association)
+
+    assign_and_remove(digits_with_six_wires, association, inverse_association, six, 6)
+
+    nine = process_with_rule(digits_with_six_wires,
+                             lambda candidate, inverse_map: True if inverse_map[4][0] in candidate and inverse_map[4][
+                                 1] in candidate and inverse_map[4][2] in candidate and inverse_map[4][
+                                                                        3] in candidate else False,
+                             inverse_association)
+
+    assign_and_remove(digits_with_six_wires, association, inverse_association, nine, 9)
+    assign_and_remove(digits_with_six_wires, association, inverse_association, digits_with_six_wires[0], 0)
+
+
+def decode_wires_for_length_five(left, association, inverse_association):
+    digits_with_five_wires = [sort(left_digit) for left_digit in left if len(left_digit) == 5]
+
+    three = process_with_rule(digits_with_five_wires,
+                              lambda candidate, inverse_map: True if inverse_map[1][0] in candidate and inverse_map[1][
+                                  1] in candidate else False, inverse_association)
+
+    assign_and_remove(digits_with_five_wires, association, inverse_association, three, 3)
+
+    five = process_with_rule(digits_with_five_wires,
+                             lambda candidate, inverse_map: True if candidate[0] in inverse_map[9] and candidate[1] in
+                                                                    inverse_map[9] and candidate[2] in inverse_map[
+                                                                        9] and candidate[3] in inverse_map[9] and
+                                                                    candidate[4] in inverse_map[9] else False,
+                             inverse_association)
+
+    assign_and_remove(digits_with_five_wires, association, inverse_association, five, 5)
+    assign_and_remove(digits_with_five_wires, association, inverse_association, digits_with_five_wires[0], 2)
+
+
+def decode_wires(rule):
+    association, inverse_association = decode_wires_for_unique(rule["left"])
+    decode_wires_for_length_six(rule["left"], association, inverse_association)
+    decode_wires_for_length_five(rule["left"], association, inverse_association)
+    return association
+
+
 def part_two(rules):
     total_sum = 0
     for rule in rules:
-        association = {[sort(left_digit) for left_digit in rule["left"] if len(left_digit) == 2][0]: 1,
-                       [sort(left_digit) for left_digit in rule["left"] if len(left_digit) == 4][0]: 4,
-                       [sort(left_digit) for left_digit in rule["left"] if len(left_digit) == 3][0]: 7,
-                       [sort(left_digit) for left_digit in rule["left"] if len(left_digit) == 7][0]: 8}
-
-        inverse_association = {1: [sort(left_digit) for left_digit in rule["left"] if len(left_digit) == 2][0],
-                               4: [sort(left_digit) for left_digit in rule["left"] if len(left_digit) == 4][0],
-                               7: [sort(left_digit) for left_digit in rule["left"] if len(left_digit) == 3][0],
-                               8: [sort(left_digit) for left_digit in rule["left"] if len(left_digit) == 7][0]}
-
-        digits_with_six_wires = [sort(left_digit) for left_digit in rule["left"] if len(left_digit) == 6]
-
-        six = [candidate for candidate in digits_with_six_wires if
-               inverse_association[1][0] not in candidate or inverse_association[1][1] not in candidate][0]
-
-        association[six] = 6
-        inverse_association[6] = six
-        digits_with_six_wires.remove(six)
-
-        nine = [candidate for candidate in digits_with_six_wires if
-                inverse_association[4][0] in candidate and inverse_association[4][1] in candidate and
-                inverse_association[4][2] in candidate and inverse_association[4][3] in candidate][0]
-
-        association[nine] = 9
-        inverse_association[9] = nine
-        digits_with_six_wires.remove(nine)
-
-        association[digits_with_six_wires[0]] = 0
-        inverse_association[0] = digits_with_six_wires[0]
-
-        digits_with_five_wires = [sort(left_digit) for left_digit in rule["left"] if len(left_digit) == 5]
-        three = [candidate for candidate in digits_with_five_wires if
-                 inverse_association[1][0] in candidate and inverse_association[1][1] in candidate][0]
-
-        association[three] = 3
-        inverse_association[3] = three
-
-        digits_with_five_wires.remove(three)
-
-        five = [candidate for candidate in digits_with_five_wires if
-                candidate[0] in inverse_association[9] and candidate[1] in inverse_association[9] and candidate[2] in
-                inverse_association[9] and candidate[3] in inverse_association[9] and candidate[4] in
-                inverse_association[9]][0]
-
-        association[five] = 5
-        inverse_association[5] = five
-
-        digits_with_five_wires.remove(five)
-
-        association[digits_with_five_wires[0]] = 2
-        inverse_association[2] = digits_with_five_wires[0]
-
+        association = decode_wires(rule)
         total_sum += int(''.join(map(str, [association[sort(right_code)] for right_code in rule["right"]])))
 
     print(total_sum)
